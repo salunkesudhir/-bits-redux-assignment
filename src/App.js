5f689187d800f15e936bfc9cb39718256c883417
sudhir salunke <sudhirsalunke@benchmarkit.solutions>
2022-01-07 02:28:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import UsersList from "./connorsGroup.Client.React/features/app/users/components/UsersList";
+
+export default class App extends Component {
+  render() {
+    return (
+      <UsersList/>
+    );
+  }
+}
